a482ff6da7fb5a82d0b32135766c8b7aebdaee9c
Jason Adams <julietmikealpha@gmail.com>
2017-09-18 15:54:07 +0100

    Checkpoint

---
+++ Assets/Scripts/ApplyForceAtOffset.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForceAtOffset : MonoBehaviour
+{
+  [SerializeField]
+  private float force = 100f;
+  [SerializeField]
+  private Vector3 offset;
+  [SerializeField]
+  private Vector3 forceDirection = Vector3.forward;
+  [SerializeField]
+  private string keyName = "Fire1";
+
+  private Rigidbody rb;
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void Update()
+  {
+    if (Input.GetButton(keyName))
+    {
+      rb.AddForceAtPosition(forceDirection.normalized * force, transform.position + offset);
+    }
+    else
+    {
+      rb.AddForceAtPosition(forceDirection.normalized * -force, transform.position + offset);
+    }
+  }
+}
---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+  [SerializeField]
+  private float force = 100.0f;
+  [SerializeField]
+  private float radius = 1.0f;
+  [SerializeField]
+  private float forceRadius = 1.0f;
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    foreach (Collider col in Physics.OverlapSphere(transform.position, radius))
+    {
+      Rigidbody rb = col.GetComponent<Rigidbody>();
+      if (rb != null)
+      {
+        rb.AddExplosionForce(force, transform.position, forceRadius);
+      }
+    }
+  }
+}
