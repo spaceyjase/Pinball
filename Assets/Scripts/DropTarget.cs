88828ab3ac34a45fb34ca0e7277e052240a5b242
Jason Adams <julietmikealpha@gmail.com>
2017-09-25 15:51:55 +0100

    Simple drop targets.

---
+++ Assets/Scripts/DropTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTarget : MonoBehaviour
+{
+  [SerializeField]
+  private float dropDistance = 1.0f;
+
+  private bool isDropped = false;
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (!isDropped)
+    {
+      transform.position += Vector3.down * dropDistance;
+      isDropped = true;
+    }
+  }
+}
