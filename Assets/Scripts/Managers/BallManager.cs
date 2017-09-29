e365a7bda7c8b5fbad015738b11755aea4db8e8c
Jason Adams <julietmikealpha@gmail.com>
2017-09-29 14:29:53 +0100

    Ball spawning.

---
+++ Assets/Scripts/Managers/BallManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+  [SerializeField]
+  private GameObject ballPrefab;
+  [SerializeField]
+  private static int lives = 3;
+  [SerializeField]
+  private Transform spawnPosition;
+
+  private void Update()
+  {
+    if (!GameObject.FindGameObjectWithTag("Ball"))
+    {
+      --lives;
+      Instantiate(ballPrefab, spawnPosition.position, ballPrefab.transform.rotation);
+    }
+  }
+}
