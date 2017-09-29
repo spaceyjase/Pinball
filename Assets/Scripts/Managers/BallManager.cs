5082560c4f9caa0ae707517a39539317f3bb4ae6
Jason Adams <julietmikealpha@gmail.com>
2017-09-29 14:52:32 +0100

    Game over.

--- Assets/Scripts/Managers/BallManager.cs
+++ Assets/Scripts/Managers/BallManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallManager : MonoBehaviour
 {
@@ -13,13 +14,36 @@ public class BallManager : MonoBehaviour
   [SerializeField]
   private TextMesh ballsRemaining;
 
+  private bool gameOver = false;
+
   private void Update()
   {
-    if (!GameObject.FindGameObjectWithTag("Ball"))
+    if (!gameOver && !GameObject.FindGameObjectWithTag("Ball"))
     {
       --lives;
-      Instantiate(ballPrefab, spawnPosition.position, ballPrefab.transform.rotation);
+      if (lives < 0)
+      {
+        lives = 0;
+      }
+
+      if (lives == 0)
+      { // shouldn't be here ;)
+        ballsRemaining.text = "GAME OVER";
+        gameOver = true;
+      }
+      else
+      { // new ball
+        Instantiate(ballPrefab, spawnPosition.position, ballPrefab.transform.rotation);
+        ballsRemaining.text = string.Empty.PadLeft(lives, '•');
+      }
     }
-    ballsRemaining.text = string.Empty.PadLeft(lives, '•');
+
+    if (gameOver)
+    {
+      if (Input.GetKeyDown(KeyCode.R))
+      {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      }
+    }
   }
 }
